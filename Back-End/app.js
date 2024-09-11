7c0470bb6552148482a060a9a907ff9bf1911a9f
Camille Michel <cammichel@sfr.fr>
2024-09-11 20:52:36 +0200

    Création premières routes et connection BDD

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Book = require('./models/book');
+
+mongoose.connect('mongodb+srv://camille_michel:Test2209@cluster0.av8esjp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
@@ -12,30 +21,37 @@ app.use((req, res, next) => {
   });
 
 app.post('/api/books', (req, res, next) => {
-console.log(req.body);
-res.status(201).json({
-    message: 'Livre ajouté !'
+    delete req.body.userId;
+    const book = new Book({
+        ...req.body
+    });
+    book.save()
+    .then(() => res.status(201).json({message: 'Livre enregistré !'}))
+    .catch(error => res.status(400).json({error}));
 });
+
+app.put('/api/books/:id', (req, res, next) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/books/:id', (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+      .then(book => res.status(200).json(book))
+      .catch(error => res.status(404).json({ error }));
 });
 
 app.get('/api/books', (req, res, next) => {
-    const books = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(books);
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({error}));
   });
 
 module.exports = app;
